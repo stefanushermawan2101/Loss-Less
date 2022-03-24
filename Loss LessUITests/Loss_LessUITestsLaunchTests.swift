e989f4ca9c97deb8228893e5c07c5d12b5fc7394
Stefanus Hermawan Sebastian <ssebastian22@bsd.idserve.net>
2022-03-24 14:24:54 +0700

    Initial Commit

---
+++ Loss LessUITests/Loss_LessUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Loss_LessUITestsLaunchTests.swift
+//  Loss LessUITests
+//
+//  Created by Stefanus Hermawan Sebastian on 24/03/22.
+//
+
+import XCTest
+
+class Loss_LessUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
