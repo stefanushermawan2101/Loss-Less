350e3ea6055231129f6456c76ec4f79248d8164d
Stefanus Hermawan Sebastian <ssebastian22@bsd.idserve.net>
2022-04-04 13:44:26 +0700

    design changed

--- Loss Less/ViewController.swift
+++ Loss Less/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
