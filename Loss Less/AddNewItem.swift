8f3702554e623e62616cf67c9bc1473880656e06
Dennis Anthony <dennisanthonyblabla@gmail.com>
2022-04-06 23:10:39 +0700

    udah bikin class buat ANI

---
+++ Loss Less/AddNewItem.swift
@@ -0,0 +1,37 @@
+//
+//  AddNewItem.swift
+//  Loss Less
+//
+//  Created by Dennis Anthony on 06/04/22.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class AddNewItem {
+
+    @IBOutlet weak var ANIExpDate: UILabel!
+    @IBOutlet weak var ANIFoodPhoto: UIImageView!
+    
+    @IBAction func ANIName(_ sender: Any) {
+    }
+    @IBAction func ANIDesc(_ sender: Any) {
+    }
+    @IBAction func ANIQty(_ sender: Any) {
+    }
+    @IBAction func ANIExpDateBtn(_ sender: Any) {
+    }
+    @IBAction func ANIFreezerBtn(_ sender: Any) {
+    }
+    @IBAction func ANIFridgeBtn(_ sender: Any) {
+    }
+    @IBAction func ANIPantryBtn(_ sender: Any) {
+    }
+    @IBAction func ANICameraBtn(_ sender: Any) {
+    }
+    @IBAction func ANISaveBtn(_ sender: Any) {
+    }
+    
+    
+}
--- Loss Less/ViewController.swift
+++ Loss Less/ViewController.swift
@@ -13,32 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    //Main Page
-    @IBOutlet weak var mainsrchBar: UISearchBar!
-    @IBOutlet weak var foodImage: UIImageView!
-    @IBOutlet weak var cndtionSymbol: UIImageView!
-    @IBOutlet weak var lblLocation: UILabel!
-    @IBOutlet weak var lblReminderDate: UILabel!
-    @IBOutlet weak var lblCreatedDate: UILabel!
-    @IBOutlet weak var lblExpDate: UILabel!
-    @IBOutlet weak var mainTblView: UITableView!
+
+    @IBOutlet weak var lblFoodnameTxt: UIButton!
+    
     @IBAction func lblFoodName(_ sender: Any) {
-    }
-    @IBAction func cndtionAll(_ sender: Any) {
-    }
-    @IBAction func cndtionExp(_ sender: Any) {
-    }
-    @IBAction func cndtionWarning(_ sender: Any) {
-    }
-    @IBAction func cndtionGood(_ sender: Any) {
-    }
-    @IBAction func mainBtnEdit(_ sender: Any) {
-    }
-    @IBAction func mainBtnAdd(_ sender: Any) {
+        lblFoodnameTxt.setTitle("test", for: .normal)
     }
     
-    //Add New Item Page
     
+    //lblFoodnameTxt.setTitle("test", for: .normal)
+        
     
     
 }
