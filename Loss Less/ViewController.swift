2196b000659bdc85401f91664cf8e10767e7b151
Dennis Anthony <dennisanthonyblabla@gmail.com>
2022-04-07 18:08:11 +0700

    udah coba munculin table view dan berhasil

--- Loss Less/ViewController.swift
+++ Loss Less/ViewController.swift
@@ -7,14 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var MainFoodTblView: UITableView!
+    
+    let fridgeArray = ["domba", "ayam", "kelinci"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+     {
+         return 1
+     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fridgeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellFridge", for: indexPath)
+                
+                cell.textLabel!.text = fridgeArray[indexPath.row]
+                
+                return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        MainFoodTblView.dataSource = self
+        MainFoodTblView.delegate = self
     }
-    @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
+
 }
 
