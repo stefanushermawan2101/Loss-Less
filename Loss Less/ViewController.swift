ffc45d0a1fa14ff702b6372834bb67d3e2c809f7
Dennis Anthony <dennisanthonyblabla@gmail.com>
2022-04-07 18:17:00 +0700

    Gua bersihin bagian yang gk perlu

--- Loss Less/ViewController.swift
+++ Loss Less/ViewController.swift
@@ -11,10 +11,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
+
 }
 
