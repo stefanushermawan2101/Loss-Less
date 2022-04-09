32150993613a7b6aa0ef0d98b63f78651388925d
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-04-09 13:16:29 +0700

    udah delete, share, checklist button

--- Loss Less/FoodTableViewController.swift
+++ Loss Less/FoodTableViewController.swift
@@ -21,7 +21,7 @@ class FoodTableViewController: UITableViewController {
     
     var foodConditions = ["warning", "good", "bad", "warning", "good", "good"]
     
-    
+    var foodEaten = Array(repeating: false, count: 6)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,6 +64,87 @@ class FoodTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80.0
     }
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete{
+//            //Delete the row from the data source
+//            foodNames.remove(at: indexPath.row)
+//            foodLocations.remove(at: indexPath.row)
+//            expDates.remove(at: indexPath.row)
+//            addedDates.remove(at: indexPath.row)
+//            foodImages.remove(at: indexPath.row)
+//            foodConditions.remove(at: indexPath.row)
+//        }
+//
+//        tableView.deleteRows(at: [indexPath], with: .fade)
+//
+//    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { action, sourceView, completionHandler in
+            //delete the row from data source
+            self.foodNames.remove(at: indexPath.row)
+            self.foodLocations.remove(at: indexPath.row)
+            self.expDates.remove(at: indexPath.row)
+            self.addedDates.remove(at: indexPath.row)
+            self.foodImages.remove(at: indexPath.row)
+            self.foodConditions.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            //call completion handler to dismiss the action button
+            completionHandler(true)
+        }
+        let shareAction = UIContextualAction(style: .normal, title: "Share") { action, sourceView, completionHandler in
+            let defaultText = "Just checking your " + self.foodNames[indexPath.row]
+            
+            let activityController: UIActivityViewController
+            
+            if let imageToShare = UIImage(named: self.foodImages[indexPath.row]) {
+//                activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+                activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            }else{
+                activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            }
+            
+            
+            
+//            let activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+            
+            self.present(activityController, animated: true, completion: nil)
+            completionHandler(true)
+        }
+        
+        deleteAction.backgroundColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 68.0/255.0, alpha: 1.0)
+        deleteAction.image = UIImage(named: "delete")
+        
+        shareAction.backgroundColor = UIColor(red: 254.0/255.0, green: 149.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+        shareAction.image = UIImage(named: "share")
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
+        
+        return swipeConfiguration
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            let checkInAction = UIContextualAction(style: .normal, title: "Check-in") { (action, sourceView, completionHandler) in
+                let cell = tableView.cellForRow(at: indexPath) as! FoodTableViewCell
+                self.foodEaten[indexPath.row] = (self.foodEaten[indexPath.row]) ? false : true
+                cell.accessoryType = (self.foodEaten[indexPath.row]) ? .checkmark : .none
+                
+                completionHandler(true)
+            }
+            
+            // Customize the action button
+            checkInAction.backgroundColor = UIColor(red: 39.0/255.0, green: 174.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+            checkInAction.image = self.foodEaten[indexPath.row] ? UIImage(named: "undo") : UIImage(named: "tick")
+            
+            let swipeConfiguration = UISwipeActionsConfiguration(actions: [checkInAction])
+            
+            return swipeConfiguration
+        }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
